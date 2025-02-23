6ff5da4d8fca0db7e85f0a20ab746c11e4aa0d75
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-02-23 21:02:15 +0100

    modifiche

--- public/index.js
+++ public/index.js
@@ -8,22 +8,29 @@ const formElement = document.getElementById("form");
 import {tableComponent} from './componenti/table.js';
 import {NavBarComponent} from './componenti/navbar.js';
 import {createForm} from './componenti/form.js';
-import {generateFetchComponent} from './componenti/fetch_component.js';
-//import { createMiddleware } from './componenti/middleware.js';
+import {createMiddleware } from './componenti/middleware.js';
+import {generatePubSub} from "./componenti/pubsub.js";
+
 
 
 fetch("conf.json").then(r => r.json()).then(conf => {
     const form = createForm(formElement);
     const table1 = tableComponent();
-    const navBarComp = NavBarComponent(conf);
-    const compFetch = generateFetchComponent()
-    compFetch.caricaDati(conf)
-    compFetch.getData().then(data => {
+    const navBarComp = NavBarComponent();
+    const pubsub = generatePubSub();
+    const middleware = createMiddleware();
+    navBarComp.setParentElement(navbar);
+
+    pubsub.subscribe("carica-dati-list", (dati) => {
         form.setLabels(data);
         table1.setData(data); // Imposta i dati nel componente tabella
         table1.setParentElement(tabella);
-        table1.render(starDay);// Renderizza la tabella con i dati recuperati
+        table1.render(starDay)
+        console.log("Dati caricati sulla lista");
     });
+
+    
+
     precendente.onclick = () => {
         starDay -= 7;
         table1.start(starDay)
@@ -35,15 +42,14 @@ fetch("conf.json").then(r => r.json()).then(conf => {
         table1.start(starDay)
         table1.render();
     }
-    navBarComp.setParentElement(navbar);
     navBarComp.render(form,table1);
     form.render(table1,compFetch)
-    setInterval(()=>{
+    /*setInterval(()=>{
         compFetch.getData().then(data => {
             form.setLabels(data);
             table1.setData(data); // Imposta i dati nel componente tabella
             table1.render(starDay);// Renderizza la tabella con i dati recuperati
             
         });
-    },300000)
+    },300000)*/
 });
